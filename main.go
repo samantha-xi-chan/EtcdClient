5bff7dafd924d17e26fed0638e049070e03a1af8
Your Name <you@example.com>
2023-10-23 15:01:55 +0800

    update

--- main.go
+++ main.go
@@ -8,39 +8,42 @@ import (
 )
 
 func main() {
-	// 客户端连接Etcd服务器
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"etcd.etcd.svc.cluster.local:2379"},
-		DialTimeout: 5 * time.Second,
-		Username:    "root",
-		Password:    "pJs3YaGdp4",
-	})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer cli.Close()
+	for true {
+		cli, err := clientv3.New(clientv3.Config{
+			Endpoints:   []string{"etcd.etcd.svc.cluster.local:2379"},
+			DialTimeout: 5 * time.Second,
+			Username:    "root",
+			Password:    "pJs3YaGdp4",
+		})
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer cli.Close()
 
-	// 存储键值对数据
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	_, err = cli.Put(ctx, "mykey", "hello world")
-	cancel()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+		// 存储键值对数据
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		_, err = cli.Put(ctx, "mykey", "hello world")
+		cancel()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
-	// 获取键值对数据
-	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
-	resp, err := cli.Get(ctx, "mykey")
-	cancel()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+		// 获取键值对数据
+		ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+		resp, err := cli.Get(ctx, "mykey")
+		cancel()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
-	// 输出获取到的值
-	for _, ev := range resp.Kvs {
-		fmt.Printf("%s : %s\n", ev.Key, ev.Value)
+		// 输出获取到的值
+		for _, ev := range resp.Kvs {
+			fmt.Printf("%s : %s\n", ev.Key, ev.Value)
+		}
+
+		time.Sleep(time.Second * 5)
 	}
 }
